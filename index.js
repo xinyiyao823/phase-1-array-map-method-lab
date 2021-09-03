36205f1d08386e518663d4ce86e125aecd9f6135
+ <+>
2021-09-02 19:30:31 -0500

    commit tree 5f7eed94550dc9efdb46f633beea9e8130ac2091

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+// const titleCased = () => {
+//   return tutorials.map(tutorial => {
+//      return tutorial.split(' ').map(word => 
+//       word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    
+//   })
+// }
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    
+  })
 }
