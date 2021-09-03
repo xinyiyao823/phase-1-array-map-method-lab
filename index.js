36224a5efa72c983b2ac9a4dcce1635af035d2e1
+ <+>
2021-09-02 19:31:04 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => 
+      word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    
+  })
 }
